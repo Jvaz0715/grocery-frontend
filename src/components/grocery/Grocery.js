46010aa631f536d199fa599d4cb08477a70e0390
Jvaz0715 <james.vazquez@codeimmersives.com>
2021-06-17 11:22:09 -0400

    adds submit functionality that adds new groceries but does not add empty inputs or repeat groceries

--- src/components/grocery/Grocery.js
+++ src/components/grocery/Grocery.js
@@ -31,11 +31,87 @@ export class Grocery extends Component {
         }
     };
 
+    handleTodoOnChange = (event) => {
+        this.setState({
+            groceryInput: event.target.value,
+            error: null,
+            errorrMessage: "",
+        });
+    };
+
+    handleOnSubmit = async (event) => {
+        event.preventDefault();
+
+        // check if there is anything in the input field, do not let the grocery div update with an empty grocery input
+
+        if(this.state.groceryInput.length === 0) {
+            this.setState({
+                error: true,
+                errorMessage: "Cannot create an empty grocery item",
+            });
+        }
+        // next, check to make sure this grocery item does not already exist in the grocery-div
+        else {
+            let groceryExists = this.state.groceryList.findIndex((item) => 
+                item.grocery.toLowerCase() === this.state.groceryInput.toLowerCase()
+            );
+
+            if (groceryExists > -1) {
+                this.setState({
+                    error: true,
+                    errorMessage: "That grocery item is already listed",
+                });
+            } else {
+                try {
+                    let createdGrocery = await axios.post(
+                        `${URL}/api/grocery-list/create-grocery`,
+                        {
+                            grocery: this.state.groceryInput,
+                        }
+                    );
+                    
+                    // now you need a new array that will take the set state grocery list and add the createdGrocery, this new array will be what you set the state to
+                    // the elipse will extend the groceryList of this.state
+                    let newGroceryArray = [...this.state.groceryList, createdGrocery.data.payload];
+
+                    this.setState({
+                        groceryList: newGroceryArray,
+                        // this will reset the input to blank rather than keep prior submit present
+                        groceryInput: "",
+                    })
+
+                } catch (e) {
+                    console.log(e);
+                }
+            }
+        }
+
+    }
+ 
     render() {
         return (
             <div>
 
+                {/* form-div */}
+                <div className="form-div">
+                    <form onSubmit={this.handleOnSubmit}>
+                        <input
+                            // note that name and id are similar but inverse 
+                            name="groceryInput" 
+                            text="text"
+                            onChange={this.handleTodoOnChange}
+                            value={ this.state.groceryInput }
+                            autoFocus
+                            id="inputGrocery" 
+                        />
+                        <button type="submit">Submit</button>
+                        <br />
+                        {/* the below will only appear if an error is triggered in handleOnSubmit */}
+                        <span style={{ color : "red" }}>{this.state.error && this.state.errorMessage}</span>
+                    </form>
+                </div>
 
+                {/* grocery-div */}
                 <div className="grocery-div">
                     <ul>
                         {this.state.groceryList.map((item, index) => {
